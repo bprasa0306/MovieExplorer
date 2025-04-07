e51a898cc806002ab129ea68b2a8f340e00d499f
bprasa0306 <bojjaprasanna2011@gmail.com>
2025-04-07 09:38:01 +0530

    removed error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {Provider} from "react-redux";
 import store from "./redux/store";
-import App from './App';
-import "./styles.css";
+import App from "./App";
+import "./Styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store = {store}>
     <App />
   </Provider>
